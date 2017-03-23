38119e831e6be0f8397a9f368275341162552e44
Alan Tse <starttodevelop@gmail.com>
2017-03-23 16:23:40 +1100

    Update Address and Resident class

--- src/Program.java
+++ src/Program.java
@@ -6,8 +6,14 @@ public class Program {
 		Resident resident = new Resident("Alan", 36, 70.0f, 170.0f, "555 555 555");
 		resident.eats();
 		
-		resident.setAddress("54",  "Fifth Street", "Toronto", "Ontario");
+		resident.setAddress("55",  "Fifty-Fifth Street", "Toronto", "Ontario");
+		System.out.println("\n");
 		System.out.println(resident.getAddress());
+		
+		resident.changeAddress("44",  "Fourty-Fourth Street", "Toronto", "Ontario");
+		System.out.println("\n");
+		System.out.println(resident.getAddress());
+
 	}
 
 }
--- src/Resident.java
+++ src/Resident.java
@@ -1,18 +1,22 @@
 //Resident sub class of the Person superclass 
 public class Resident extends Person {
-    // Private instance variables
+	// Private instance variables
 	private String _SIN;
 	private Address _address;
-	
+
 	// Getters and Setters
 	public String getSIN() {
 		return this._SIN;
 	}
-	public void setAddress(String streetNumber, String streetName, 
-			               String cityName, String provinceName) {
+
+	public void setAddress(String streetNumber, String streetName, String cityName, String provinceName) {
 		this._address = new Address(streetNumber, streetName, cityName, provinceName);
+
 	}
-	
+
+	public void changeAddress(String streetNumber, String streetName, String cityName, String provinceName) {
+		this._address.change(streetNumber, streetName, cityName, provinceName);
+	}
 	public String getAddress() {
 		try {
 			return this._address.toString();
@@ -21,12 +25,12 @@ public class Resident extends Person {
 			return "Error: Address not initialized";
 		}
 	}
-	
+
 	// Constructor
 	Resident(String name, int age, float heightInInches, float massInPounds, String SIN) {
 		super(name, age, heightInInches, massInPounds);
 		this._SIN = SIN;
-		
+
 	}
 
 }
